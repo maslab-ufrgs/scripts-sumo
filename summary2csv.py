47e97a4ffacaeb5325aab3c3e9d4c3dceaedace4
andersonrochatavares@gmail.com <andersonrochatavares@gmail.com@8110ab25-4fe2-f7c8-391c-19ccc05fb69e>
2013-01-28 18:15:14 +0000

    now plotting waiting/(waiting+emitted) AND waiting/total if the user desires so

--- summary2csv.py
+++ summary2csv.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
                             help="the output file (mandatory)")
     optParser.add_option("-s", "--separator", default=" ", type='str',
                             help="the separator for the output file (default=whitespace")
+    optParser.add_option("-t", "--total", default=0, type='int',
+                            help="the total number of vehicles in the simulation [use if you need to normalize waiting/total, otherwise normalization will be waiting/(waiting+emitted)]")
 
     (options, args) = optParser.parse_args()
     
@@ -23,25 +25,37 @@ if __name__ == '__main__':
     out = open(options.outfile,'w')
     xmltree = ET.parse(options.xmlfile)
     
-    out.write(sep.join(
-            ['#time','loaded','emitted','running','waiting',
-            'ended','meanWaitingTime','meanTravelTime','duration','norm-waiting']
-        ) + '\n'
-    )
+    header = ['#time','loaded','emitted','running','waiting',
+            'ended','meanWaitingTime','meanTravelTime','duration','wait/(wait+emit)']
+    
+    if options.total > 0:
+        header += ['wait/tot']
+    
+    out.write(sep.join(header) + '\n')
+    
     for element in xmltree.getroot():
         waiting = int(element.get('waiting'))
-        waitingPlusEmitted = waiting + int(element.get('emitted'))
-        out.write(sep.join(['%s','%s','%s','%s','%s','%s','%s','%s','%s', '%s\n']) % ( 
-                  element.get('time'),
-                  element.get('loaded'),
-                  element.get('emitted'),
-                  element.get('running'),
-                  element.get('waiting'),
-                  element.get('ended'),
-                  element.get('meanWaitingTime'),
-                  element.get('meanTravelTime'),
-                  element.get('duration'),
-                  float(waiting) / waitingPlusEmitted
-                  
-        ))
+        waiting_over_wannadepart = float(waiting) / (waiting + int(element.get('emitted')))
+        
+        
+        places = ['%s','%s','%s','%s','%s','%s','%s','%s','%s', '%s']
+        variables = (element.get('time'),
+              element.get('loaded'),
+              element.get('emitted'),
+              element.get('running'),
+              element.get('waiting'),
+              element.get('ended'),
+              element.get('meanWaitingTime'),
+              element.get('meanTravelTime'),
+              element.get('duration'),
+              waiting_over_wannadepart
+        )
+        
+        if options.total > 0:
+            waiting_over_total =  float(waiting) / options.total
+            places += ['%s']
+            variables += (waiting_over_total,)
+    
+        
+        out.write(sep.join(places + ['\n']) % variables)
         
