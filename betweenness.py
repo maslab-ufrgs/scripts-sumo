21d0bb8d2a8ef091838677ef856b8545e2e732db
cristianogalafassi@gmail.com <cristianogalafassi@gmail.com@8110ab25-4fe2-f7c8-391c-19ccc05fb69e>
2013-04-17 12:01:53 +0000

    Intregação com o script de leitura da rede

--- betweenness.py
+++ betweenness.py
@@ -1,91 +1,146 @@
 '''
 Created on 08/04/2013
 
-@author: Gabriel Ramos
+@authors: Cristiano Galafassi, Gabriel Ramos, Rodrigo Batista
 '''
 import sys
 import xml.etree.ElementTree as ET
 from optparse import OptionParser
 from igraph import *
+import sumolib
+import sys, getopt
+debug = True
 
-def getGraph():
-    g = Graph()
-    g.add_vertices(["A1","B1","A","B","C","D","E","F","G","H","I","J","K","L","M","L1","M1"])
-    g.add_edges([("A1","A"),("A","A1"),("B1","B"),("B","B1"),("A","B"),("B","A"),("A","C"),("C","A"),("A","D"),("D","A"),
-                 ("B","D"),("D","B"),("B","E"),("E","B"),("C","D"),("D","C"),("C","F"),("F","C"),("C","G"),("G","C"),
-                 ("D","E"),("E","D"),("D","G"),("G","D"),("D","H"),("H","D"),("E","H"),("H","E"),("F","G"),("G","F"),
-                 ("F","I"),("I","F"),("G","J"),("J","G"),("G","H"),("H","G"),("H","K"),("K","H"),("I","L"),("L","I"),
-                 ("I","J"),("J","I"),("J","K"),("K","J"),("J","L"),("L","J"),("J","M"),("M","J"),("K","M"),("M","K"),
-                 ("L","L1"),("L1","L"),("M","M1"),("M1","M")]);
-    g.es["name"] = ["A1A","AA1","B1B","BB1","AB","BA","AC","CA","AD","DA","BD","DB","BE","EB",
-                    "CD","DC","CF","FC","CG","GC","DE","ED","DG","GD","DH","HD","EH","HE",
-                    "FG","GF","FI","IF","GJ","JG","GH","HG","HK","KH","IL","LI","IJ","JI",
-                    "JK","KJ","JL","LJ","JM","MJ","KM","MK","LL1","L1L","MM1","M1M"];
-    return g
+def printdebug(arr):
+    for a in arr:
+        print a
 
-def calc_Betweenness(in_file, out_file, separator, topological, OD, routes):
-    infile = ET.parse('ortuzar.rou.xml')
-    table = {}
+def writefile(names, bet, out_file, separator):
+    #Generate the output file
+    out = open(out_file, 'w')
+    out.write('#Edge' + separator + 'Betweenness\n')
     
-    #initialize table with edges 
-    g = getGraph()
-    for edge in g.es["name"]:
-        table[edge] = [1, 0, 0]
+    #Print values to file
+    if len(names) != len(bet):
+        print "ERRO - Betweenness length differs from names length!"
+    else:
+        for i in range(0,len(names)-1):
+            out.write(str(names[i]) + separator + str(bet[i]) + '\n')
     
-    #count the number of times each edge appears in a route/OD pair
-    for vehicle in infile.getroot():
-        route = vehicle[0].get('edges').split(' ')
-        for element in range(len(route)):
-            edge = route[element]
-            sumOD = 1 if element in [0,len(route)-1] else 0 
-            #if edge not in table:
-            #    table[edge] = [1, 1, sumOD]
-            #else:
-            table[edge] = [1, table[edge][1]+1, table[edge][2]+sumOD]
+    #Close file
+    out.close()
+
+def calc_Betweenness(network_file, routes_file, out_file, separator, topological, OD, routes, weighted):
     
-    #calculate betweenness
-    wlR = [1]*len(g.es["name"])
-    wlOD = [1]*len(g.es["name"])
-    for e in range(len(g.es["name"])):
-        edge = g.es["name"][e]
-        if edge in table:
-            wlR[e] = table[edge][0] + table[edge][1]
-            wlOD[e] = table[edge][0] + table[edge][2]
-    bR = g.edge_betweenness(weights=wlR)
-    bOD = g.edge_betweenness(weights=wlOD)
+    #Load the network file
+    roadnetwork = sumolib.net.readNet(network_file)
+    roadnetwork_edges = roadnetwork.getEdges()
+    roadnetwork_nodes = roadnetwork.getNodes()
     
-    #update table with betweenness values
-    for e in range(len(g.es["name"])):
-        edge = g.es["name"][e]
-        if edge in table:
-            table[edge][1] = bR[e]
-            table[edge][2] = bOD[e]
+    #Initialize some variables
+    nodes = []
+    node_names = []
+    edges = []
+    edge_weights = []    
+    edge_names = []
     
-    #generate the output file(s)
-    if routes:
-        outR = open(out_file + '.routes.csv', 'w')
-        outR.write('#Edge' + separator + 'Betweenness\n')
+    #Read edges from the network file
+    for e in roadnetwork_edges:
+        edges += [(e.getFromNode().getID(), e.getToNode().getID())]
+        edge_names += [e.getID()]
+        
+        if weighted:
+            #If edge does not have any lanes, put constant weight
+            if e.getLaneNumber()==0:
+                edge_weights += [1]
+            else:
+                edge_weights += [e.getLength() / e.getLaneNumber()]
+        else:
+            edge_weights += [1]
+            
+    #Read nodes from the network file
+    for n in roadnetwork_nodes:
+        nodes += [n]
+        node_names += [n.getID()]
+    
+    if debug:
+        print "NODES:"
+        printdebug(nodes)
+        printdebug(node_names)
+        print "EDGES:"
+        printdebug(edges)
+        printdebug(edge_names)
+        printdebug(edge_weights)
+
+    #Load igraph graph structure
+    g = Graph(directed = True)
+    for n in nodes:
+        g.add_vertex(n.getID())
+    
+    g.add_edges(edges)
+    g.es["name"] = edge_names
+    g.es["weight"] = edge_weights
+
+    if debug:
+        print "EDGES IGRAPH"
+        printdebug(g.get_edgelist())
+        print "EDGES IGRAPH NAMES"
+        printdebug(g.es["name"])
+        #Print betweenness edge information        
+        print "edge - betweenness"
+        #If --weighted is false, then is weight=1 otherwise wieght=length/lanesnumber
+        print g.edge_betweenness(directed=True, cutoff=None, weights=g.es["weight"])
+    
+    if topological:
+        bT = g.edge_betweenness(directed=True, cutoff=None, weights=g.es["weight"])
+    
+    if OD or routes:
+        infile = ET.parse(routes_file)
+        table = {}
+        
+        #initialize table with edges 
+        for edge in g.es["name"]:
+            table[edge] = [1, 0, 0]
+        
+        #count the number of times each edge appears in a route/OD pair
+        for vehicle in infile.getroot():
+            route = vehicle[0].get('edges').split(' ')
+            for element in range(len(route)):
+                edge = route[element]
+                sumOD = 1 if element in [0,len(route)-1] else 0 
+                table[edge] = [1, table[edge][1]+1, table[edge][2]+sumOD]
+        
+        #calculate betweenness
+        wlR = [1]*len(g.es["name"])
+        wlOD = [1]*len(g.es["name"])
+        for e in range(len(g.es["name"])):
+            edge = g.es["name"][e]
+            if edge in table:
+                wlR[e] = table[edge][0] + table[edge][1]
+                wlOD[e] = table[edge][0] + table[edge][2]
+        bR = g.edge_betweenness(directed=True, cutoff=None, weights=wlR)
+        bOD = g.edge_betweenness(directed=True, cutoff=None, weights=wlOD)
+        
+    #Print topological betweenness
+    out = out_file+'.topological.csv'
+    if topological:
+        writefile(g.es["name"], bT, out, separator)
+    
     if OD:
-        outOD = open(out_file + '.OD.csv', 'w')
-        outOD.write('#Edge' + separator + 'Betweenness\n')
-    
-    #print values to files
-    for element in table:
-        if routes:
-            outR.write(element + separator + str(table[element][1]) + '\n')
-        if OD:
-            outOD.write(element + separator + str(table[element][2]) + '\n')
+        writefile(g.es["name"], bOD, out_file+'.od.csv', separator)
     
     if routes:
-        outR.close()
-    if OD:
-        outOD.close()
+        writefile(g.es["name"], bR, out_file+'.routes.csv', separator)
+    
+
     
 if __name__ == '__main__':
     optParser = OptionParser()
     
+    optParser.add_option("-n", "--network-file", dest='networkfile',
+                            help="sumo network file (mandatory)")
     optParser.add_option("-r", "--routes-file", dest='routesfile',
-                            help="the routes file to which the betweenness should be calculated (mandatory)")
+                            help="the routes file to which the betweenness should be calculated")
     optParser.add_option("-c", "--output-csv", default="out", dest='csvfile',
                             help="the output CSV file, without extension (default=out)")
     optParser.add_option("-s", "--separator", default=" ", type='str',
@@ -96,15 +151,23 @@ if __name__ == '__main__':
                             help="OD pairs analysis")
     optParser.add_option("-R", "--routes", default=False, action="store_true",
                             help="routes analysis")
+    optParser.add_option("-W", "--weighted", default=False, action="store_true",
+                            help="Weighted: Length / LaneNumber. Else 1 ")
+    
     
     (options, args) = optParser.parse_args()
     
-    if options.routesfile == None:
+    if options.networkfile == None:
+        print "The network file must be informed"
+        sys.exit() 
+
+    if (options.OD or options.routes) and options.routesfile == None:
         print "The routes file must be informed"
         sys.exit()
     
-    if options.topological == False and options.OD == False and options.routes == False:
+    if not options.topological and  not options.OD and not options.routes:
         print "Specify at least one type of analysis (topological, OD pairs, routes)"
         sys.exit()
-        
-    calc_Betweenness(options.routesfile, options.csvfile, options.separator, options.topological, options.OD, options.routes)
+    
+    
+    calc_Betweenness(options.networkfile, options.routesfile, options.csvfile, options.separator, options.topological, options.OD, options.routes, options.weighted)
