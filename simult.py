bda19ce6c07ea603b58cb909d063fc18fdcd38ec
andersonrochatavares@gmail.com <andersonrochatavares@gmail.com@8110ab25-4fe2-f7c8-391c-19ccc05fb69e>
2013-05-09 17:46:52 +0000

    last parameter of simult can be omitted, it defaults to zero

--- simult.py
+++ simult.py
@@ -3,6 +3,7 @@ Makes all drivers have the same depart time.
 
 Receives 3 args: the input.rou.xml file, the output.rou.xml file and the depart time
 
+Last parameter is defaulted to 0.00 if omitted
 '''
 
 import xml.etree.ElementTree as ET
@@ -10,9 +11,10 @@ import sys
 
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
+depart_time = sys.argv[3] if len(sys.argv) >= 4 else '0.00'
 
 for element in root:
-    element.set('depart',sys.argv[3] or '0.00')
+    element.set('depart', depart_time)
     
     
 tree.write(sys.argv[2])
